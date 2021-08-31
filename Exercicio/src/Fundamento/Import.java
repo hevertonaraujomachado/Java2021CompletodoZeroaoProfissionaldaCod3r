23962b72356ee0e49f4cfe736300a5041146a53d
Heverton Araujo Machado <71562525+hevertonaraujomachado@users.noreply.github.com>
2021-08-31 14:32:11 -0300

    Add files via upload

---
+++ Exercicio/src/Fundamento/Import.java
@@ -0,0 +1,23 @@
+package Fundamentos;
+
+import java.util.Date;
+
+import javax.swing.JButton;
+
+public class Import {
+
+	public static void main(String[] args) {
+		
+		String CampeãodaLibertadores = "Palmeiras";
+		System.out.println(CampeãodaLibertadores);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		JButton botao = new JButton();
+		System.out.println();
+		
+
+	}
+
+}
